b583ab2e452fb940ea709bee8cb771af4b7143ff
idevyogi <urisumit@gmail.com>
2025-07-16 13:51:04 +0530

    Update DAY_1_Beginner_working_with_variables.py

--- DAY 1 BASICS.py
+++ DAY 1 BASICS.py
@@ -1,1 +1,34 @@
+# Basics of python
+# use stack overflow  site for error correction
+# in python spaces are really very very important
+print("Hello World")
+
+# STRING MANIPULATION
+#Give New line to next hello world
+print("Hello World\nHello world")
+
+# In concatentation just string can concatenate not others data types
+print("Hello " + "Angela")
+print("Hello" + " " + "Angela")
+
+
+# Python input function - input()
+input("What is your name")
+
+# in this first it will compile input then print (hello + input )
+print("Hello" + input("What is your name?"))
+
+# Thonny is available for windows it is helpful in understanding how compiler reading code
+# Appliction link - https://thonny.org
+
+# Python Variables (Variable is temporary value holder contianer)
+name = input("What is your name?")
+print(name)
+
+#use len() for Python string length
+print(len(name))
+print(len(input("What is your name?")))
+
+
+
 
