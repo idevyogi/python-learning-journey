4859ef196ed1548baf26d2731e3343c55a2ab329
idevyogi <urisumit@gmail.com>
2025-07-18 18:45:15 +0530

    Update DAY 3 BASICS.py

--- DAY 3 BASICS.py
+++ DAY 3 BASICS.py
@@ -25,3 +25,40 @@ if b == 0 :
     print("it is even number")
 else:
     print("it is odd number")
+
+
+#  Nested if statements and elif statements
+
+'''
+if condition:
+   if another condition:
+      do this
+   else:
+     do this
+else:
+    do this
+'''
+
+print("Welcome to the rollercoaster")
+height= int(input("What is your height in cm ? "))
+if height > 120:
+    print("You cant ride the rollercoaster")
+    age = int(input("What is your age"))
+    if age <= 18 :
+        print("Please pay $7.")
+    else:
+        print("Please pay $12")
+else:
+    print("Sorry you have to grow taller")
+
+#  if/elif/else
+
+# if condition 1:
+#    do A
+# elif condition 2:
+#     do B
+# else:
+#     do this
+
+
+
